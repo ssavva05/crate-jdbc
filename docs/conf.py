cace4dcc6512652d6b9ef2537cb68fb98d8279d0
Fabian Reisegger <fabian@crate.io>
2017-07-11 17:07:02 +0200

    fixup! fixup! wip: refactored bootstrap process of sphinx build environment

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 from crate.theme.rtd.conf.crate_server import *
 
-exclude_patterns = ['.venv/**', 'out/**', 'tmp/**', 'eggs/**', 'requirements.txt']
+exclude_patterns = ['.venv/**', 'out/**', 'tmp/**', 'eggs/**', 'requirements.txt', 'requirements-raw.txt']
 
 # crate.theme sets html_favicon to favicon.png which causes a warning because it should be a .ico
 # and in addition there is no favicon.png in this project so it can't find the file
