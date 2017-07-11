87df5292a1f69f53b737c89d18741a7c60116869
Michael Beer <michael@crate.io>
2017-07-11 16:48:28 +0200

    wip: refactored bootstrap process of sphinx build environment

---
+++ docs/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if hash python3.6 2> /dev/null; then
+    python3.6 -m venv .venv
+elif hash python3 2> /dev/null; then
+    # fallback to python3 in case there is no python3.6 alias; should be 3.6
+    python3 -m venv .venv
+else
+    echo 'python3.6 required'
+    exit 1
+fi
+
+.venv/bin/python -m pip install -U pip setuptools
+.venv/bin/python -m pip install -r requirements.txt
